95f8ca89907faa311c258236fbd3f26b326a27bc
Trent Smith <trsmith@1-D-29.42heilbronn.de>
2024-05-27 14:16:08 +0200

    a

---
+++ libs.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libs.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trsmith <trsmith@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 14:10:13 by trsmith           #+#    #+#             */
+/*   Updated: 2024/05/27 14:10:15 by trsmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBS_H
+# define LIBS_H
+
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
+
+#endif
